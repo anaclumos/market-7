bf8cfd0904f4556e125e0d6be97d9e0cff292e6c
eunjung <jenny.it.kr@gmail.com>
2020-07-06 17:11:37 +0900

    Add pug files for home, register

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import express from "express";
+import path from "path";
+import pug from "pug";
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("안녕!");
-});
+app.set("view engine", "pug");
+
+app.get("/register", (req, res) => res.render("register"));
+app.get("/", (req, res) => res.render("home"));
+
 
 const PORT = 4000;
 
