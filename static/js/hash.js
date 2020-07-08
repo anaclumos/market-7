57e1b8ce05be53a49ff7c6d33ce45a4bae99e45f
Sunghyun Cho <mail@chosunghyun.com>
2020-07-08 16:17:51 +0900

    Fix Bug in hash function
    * hash의 버그 수정
    * hash를 외부에서 사용할 수 없는 버그 수정

--- static/js/hash.js
+++ static/js/hash.js
@@ -1,6 +1,6 @@
 import crypto from "crypto";
 
-var config = {
+const config = {
   hashBytes: 64,
   saltBytes: 64,
   iterations: 872791
@@ -10,19 +10,16 @@ export function generateRandomSalt() {
   return crypto.randomBytes(config.saltBytes).toString('base64')
 }
 
-export async function hash(input, salt) {
+export function hash(input, salt) {
   return new Promise((resolve, reject) => {
     try {
-      crypto.pbkdf2(input, salt, config.iterations, config.hashBytes, 'sha512', (err, key => {
+      crypto.pbkdf2(input, salt, config.iterations, config.hashBytes, 'sha512', (err, key) => {
         if (err) reject(e)
         resolve(key.toString('base64'));
-      }));
+      });
     } catch (e) {
       console.log("hashWithString err: " + e);
       reject(e);
     }
   });
 }
-
-// const passwordSalt = generateRandomSalt();
-// const passwordHash = hash(password, passwordSalt);
