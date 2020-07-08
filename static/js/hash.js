0957005acc757cbc58851786e1d91529ebbf9eec
Sunghyun Cho <mail@chosunghyun.com>
2020-07-08 14:25:26 +0900

    Create hash function
    * Separate hash config

--- static/js/hash.js
+++ static/js/hash.js
@@ -1,5 +1,25 @@
 import crypto from "crypto";
 
-function generateRandomString(len) {
-  return crypto.randomBytes(len).toString('base64')
+var config = {
+  hashBytes: 64,
+  saltBytes: 16,
+  iterations: 872791
+};
+
+function generateRandomString() {
+  return crypto.randomBytes(config.saltBytes).toString('base64')
+}
+
+async function hash(input, salt) {
+  return new Promise((resolve, reject) => {
+    try {
+      crypto.pbkdf2(input, salt, config.iterations, config.hashBytes, 'sha512', (err, key => {
+        if (err) reject(e)
+        resolve(key.toString('base64'));
+      }));
+    } catch (e) {
+      console.log("hashWithString err:," + e);
+      reject(e);
+    }
+  });
 }
