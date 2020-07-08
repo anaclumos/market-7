1f4e1d22a0721cc0830b5dd68b843437627b3a15
eunjung <jenny.it.kr@gmail.com>
2020-07-08 13:33:57 +0900

    Add terms checkbox handler
    - 전체 약관 동의 토글시  개별 약관 동의 여부 연동
    - 개별 약관 토글시 전체 약관 동의 여부 연동

--- static/js/terms.js
+++ static/js/terms.js
@@ -3,17 +3,28 @@ const allTermsCheck = terms.querySelector(".term__all input");
 const mendatoryTermsCheck = terms.querySelector(".term__mendatory input");
 const optionalTermsChecks = terms.querySelectorAll(".term__optional input");
 
-const toggleAllCheckBoxes = function(e) {
-  const isChecked = e.target.checked;
-  if (isChecked) {
-    allTermsCheck.setAttribute("checked", "");
-    mendatoryTermsCheck.setAttribute("checked", "");
-    optionalTermsChecks.forEach(checkBox => checkBox.setAttribute("checked", ""));
+const termsCheckboxHandler = function(e) {
+  if (!["INPUT", "SPAN"].includes(e.target.tagName)) return;
+  const selectedTerm = e.target.parentElement;
+  const selectedCheckbox = selectedTerm.querySelector("input");
+  const updatedStatus = !selectedCheckbox.checked;
+  if (e.target.tagName === "SPAN") {
+    selectedCheckbox.checked = updatedStatus;
+  }
+  if (Array.from(selectedTerm.classList).includes("term__all")) {
+    mendatoryTermsCheck.checked = updatedStatus;
+    optionalTermsChecks.forEach(check => check.checked = updatedStatus);
   } else {
-    allTermsCheck.removeAttribute("checked");
-    mendatoryTermsCheck.removeAttribute("checked");
-    optionalTermsChecks.forEach(checkBox => checkBox.removeAttribute("checked"));
+    if (updatedStatus) {
+      if (mendatoryTermsCheck.checked && Array.from(optionalTermsChecks).every(check => check.checked)) {
+        // 체크가 된 경우에는 다른 나머지 버튼들이 모두 체크돼어있는지 확인하고 그런 경우에는 전체 동의 체크
+        allTermsCheck.checked = updatedStatus;
+      }
+    } else {
+      // 체크가 해제된 경우에는 전체 동의 체크 해제
+      allTermsCheck.checked = updatedStatus;
+    }
   }
 }
 
-allTermsCheck.addEventListener("click", toggleAllCheckBoxes);
+terms.addEventListener("click", termsCheckboxHandler);
