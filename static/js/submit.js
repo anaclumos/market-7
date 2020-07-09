f39a399e8605ebe531c5dbbbc6d6509204982127
eunjung <jenny.it.kr@gmail.com>
2020-07-09 20:42:06 +0900

    Refactor submit
    - 폼 내부에서 엔터 입력 또는 가입하기 버튼을 포함한 모든 버튼 클릭시 reload 되지 않도록 수정
    - 가입하기 버튼 클릭시, 모든 입력값의 검증 통과 여부 확인 후 요청을 전송하는 함수 handleSubmitBtnClick 생성

--- static/js/submit.js
+++ static/js/submit.js
@@ -1,14 +1,26 @@
+import { isMandatoryArgsValid } from "./isInputsValid.js"
 
-const registerForm = document.querySelector("form");
+(function(){
+  
+  const registerForm = document.querySelector("form");
+  const submitBtn = registerForm.querySelector("#submit-btn");
 
-
-const preventSubmission = function(e) {
-  const isEnterPressed = (e.type === "keydown" && e.key === "Enter");
-  const isBtnClicked = (e.type === "click" && e.target.tagName === "BUTTON");
-  if (isEnterPressed || isBtnClicked) {
-    e.preventDefault();
+  const preventSubmission = function(e) {
+    const isEnterPressed = (e.type === "keydown" && e.key === "Enter");
+    const isNonSubmitBtnClicked = (e.type === "click" && e.target.tagName === "BUTTON");
+    if (isEnterPressed || isNonSubmitBtnClicked) {
+      e.preventDefault();
+    }
   }
-}
 
-registerForm.addEventListener("keydown", preventSubmission);
-registerForm.addEventListener("click", preventSubmission);
+  const handleSubmitBtnClick = function() {
+    const isSubmittable = Object.values(isMandatoryArgsValid).every(elem => elem);
+    if (isSubmittable) {
+      console.log("send POST request")
+    }
+  }
+
+  registerForm.addEventListener("keydown", preventSubmission);
+  registerForm.addEventListener("click", preventSubmission);
+  submitBtn.addEventListener("click", handleSubmitBtnClick);
+})();
