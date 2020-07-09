1c2c0ecc9cef33faec3609898a89a71c1f8e00de
eunjung <jenny.it.kr@gmail.com>
2020-07-10 00:08:43 +0900

    Fix terms.js
      - 체크 박스 연동 조건 수정 (필수 약관이 여러 개인 경우까지 커버
      - 즉시실행함수로 수정

--- static/js/terms.js
+++ static/js/terms.js
@@ -1,30 +1,34 @@
-const terms = document.querySelector(".terms");
-const allTermsCheck = terms.querySelector(".term__all input");
-const mendatoryTermsCheck = terms.querySelector(".term__mendatory input");
-const optionalTermsChecks = terms.querySelectorAll(".term__optional input");
+(function() {
+  const terms = document.querySelector(".terms");
+  const allTermsCheck = terms.querySelector(".term__all input");
+  const mendatoryTermsChecks = terms.querySelectorAll(".term__mendatory input");
+  const optionalTermsChecks = terms.querySelectorAll(".term__optional input");
 
-const termsCheckboxHandler = function(e) {
-  if (!["INPUT", "SPAN"].includes(e.target.tagName)) return;
-  const selectedTerm = e.target.parentElement;
-  const selectedCheckbox = selectedTerm.querySelector("input");
-  const updatedStatus = !selectedCheckbox.checked;
-  if (e.target.tagName === "SPAN") {
-    selectedCheckbox.checked = updatedStatus;
-  }
-  if (Array.from(selectedTerm.classList).includes("term__all")) {
-    mendatoryTermsCheck.checked = updatedStatus;
-    optionalTermsChecks.forEach(check => check.checked = updatedStatus);
-  } else {
-    if (updatedStatus) {
-      if (mendatoryTermsCheck.checked && Array.from(optionalTermsChecks).every(check => check.checked)) {
-        // 체크가 된 경우에는 다른 나머지 버튼들이 모두 체크돼어있는지 확인하고 그런 경우에는 전체 동의 체크
+  const termsCheckboxHandler = function(e) {
+    if (!["INPUT", "SPAN"].includes(e.target.tagName)) return;
+    const selectedTerm = e.target.parentElement;
+    const selectedCheckbox = selectedTerm.querySelector("input");
+    const updatedStatus = selectedCheckbox.checked;
+    if (e.target.tagName === "SPAN") {
+      selectedCheckbox.checked = updatedStatus;
+    }
+    if (selectedTerm === allTermsCheck.parentElement) {
+      mendatoryTermsChecks.forEach(check => check.checked = updatedStatus);
+      optionalTermsChecks.forEach(check => check.checked = updatedStatus);
+    } else {
+      if (updatedStatus) {
+        const isAllMendatoryTermsChecked = Array.from(mendatoryTermsChecks).filter(check => check !== e.target).every(check => check.checked);
+        const isAllOptionalTermsChecked = Array.from(optionalTermsChecks).filter(check => check !== e.target).every(check => check.checked);
+        if (isAllMendatoryTermsChecked && isAllOptionalTermsChecked) {
+          // 체크가 된 경우에는 다른 나머지 버튼들이 모두 체크돼어있는지 확인하고 그런 경우에는 전체 동의 체크
+          allTermsCheck.checked = updatedStatus;
+        }
+      } else {
+        // 체크가 해제된 경우에는 전체 동의 체크 해제
         allTermsCheck.checked = updatedStatus;
       }
-    } else {
-      // 체크가 해제된 경우에는 전체 동의 체크 해제
-      allTermsCheck.checked = updatedStatus;
     }
   }
-}
 
-terms.addEventListener("click", termsCheckboxHandler);
+  terms.addEventListener("click", termsCheckboxHandler);
+})();
