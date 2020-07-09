e50544a46a26dc8b039a2a8232ff3f0b7f79cc1c
eunjung <jenny.it.kr@gmail.com>
2020-07-10 00:08:59 +0900

    Refactor optionalInfo.js
      - 즉시실행함수로 수정

--- static/js/optionalInfo.js
+++ static/js/optionalInfo.js
@@ -1,43 +1,45 @@
-const optionalWrapper = document.querySelector(".optional");
-const optionalCheckWrapper = optionalWrapper.querySelector(".optional__check");
-const addressInfoWrapper = optionalWrapper.querySelector(".optional__address");
+(function() {
+  const optionalWrapper = document.querySelector(".optional");
+  const optionalCheckWrapper = optionalWrapper.querySelector(".optional__check");
+  const addressInfoWrapper = optionalWrapper.querySelector(".optional__address");
 
-const activateInput = function() {
-  addressInfoWrapper.querySelectorAll("*").
-    forEach(elem => {
-      if (["INPUT", "BUTTON"].includes(elem.tagName)) {
-        elem.removeAttribute("disabled");
-        if (elem.tagName === "BUTTON") {
-          elem.classList.remove("deactive");
-          elem.classList.add("unfilled");
+  const activateInput = function() {
+    addressInfoWrapper.querySelectorAll("*").
+      forEach(elem => {
+        if (["INPUT", "BUTTON"].includes(elem.tagName)) {
+          elem.removeAttribute("disabled");
+          if (elem.tagName === "BUTTON") {
+            elem.classList.remove("deactive");
+            elem.classList.add("unfilled");
+          }
         }
-      }
-    })
-}
-
-const deactivateInput = function() {
-  addressInfoWrapper.querySelectorAll("*").
-    forEach(elem => {
-      if (["INPUT", "BUTTON"].includes(elem.tagName)) {
-        elem.setAttribute("disabled", "");
-        if (elem.tagName === "BUTTON") {
-          elem.classList.remove("unfilled");
-          elem.classList.add("deactive");
-        }
-      }
-    })
-}
-
-const checkboxHandler = function(e) {
-  const checkbox = optionalCheckWrapper.querySelector("input");
-  if (e.target.tagName === "SPAN") {
-    checkbox.checked = !checkbox.checked;
-  };
-  if (checkbox.checked) {
-    activateInput();
-  } else {
-    deactivateInput();
+      })
   }
-}
 
-optionalCheckWrapper.addEventListener("click", checkboxHandler);
+  const deactivateInput = function() {
+    addressInfoWrapper.querySelectorAll("*").
+      forEach(elem => {
+        if (["INPUT", "BUTTON"].includes(elem.tagName)) {
+          elem.setAttribute("disabled", "");
+          if (elem.tagName === "BUTTON") {
+            elem.classList.remove("unfilled");
+            elem.classList.add("deactive");
+          }
+        }
+      })
+  }
+
+  const checkboxHandler = function(e) {
+    const checkbox = optionalCheckWrapper.querySelector("input");
+    if (e.target.tagName === "SPAN") {
+      checkbox.checked = !checkbox.checked;
+    };
+    if (checkbox.checked) {
+      activateInput();
+    } else {
+      deactivateInput();
+    }
+  }
+
+  optionalCheckWrapper.addEventListener("click", checkboxHandler);
+})();
