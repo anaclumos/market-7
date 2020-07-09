7eb071d00263b187dc89c67800a39e29555921aa
eunjung <jenny.it.kr@gmail.com>
2020-07-09 20:39:39 +0900

    Refactor validator
    - validation 을 위한 조건, 설정 등에 관한 변수 validationConfig 생성
    - 여러가지 입력값 검증에 통합적으로 사용할 수 있는 함수 checkValidity 생성
    - 가입 요청 전송 전 모든 입력값이 조건에 부합하는지 검사하기 위한 객체 isMandatoryArgsValid 생성
    - js 파일간 변수명 중복 방지를 위해 즉시 실행 함수 형식으로 변경

--- static/js/register.js
+++ static/js/register.js
@@ -1,6 +1,7 @@
-import "./validators.js";
+import "./validator.js";
 import "./terms.js";
 import "./optionalInfo.js";
 import "./map.js";
 import "./telAuth.js";
 import "./submit.js";
+import "./isInputsValid.js";
