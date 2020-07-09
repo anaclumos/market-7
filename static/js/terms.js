939abed0fe84b571b296c80ae650736656730921
eunjung <jenny.it.kr@gmail.com>
2020-07-10 03:11:05 +0900

    Fix terms.js
    - 체크 박스간 연동시 버그 수정
    - 체크박스를 직접 클릭한 경우만 핸들링하는 것으로 변경

--- static/js/terms.js
+++ static/js/terms.js
@@ -1,25 +1,23 @@
 (function() {
   const terms = document.querySelector(".terms");
   const allTermsCheck = terms.querySelector(".term__all input");
-  const mendatoryTermsChecks = terms.querySelectorAll(".term__mendatory input");
-  const optionalTermsChecks = terms.querySelectorAll(".term__optional input");
+  const mandatoryTermsContainer = terms.querySelectorAll(".term__mandatory");
+  const optionalTermsContainer = terms.querySelectorAll(".term__optional");
 
   const termsCheckboxHandler = function(e) {
-    if (!["INPUT", "SPAN"].includes(e.target.tagName)) return;
+    if (e.target.tagName !== "INPUT") return;
     const selectedTerm = e.target.parentElement;
     const selectedCheckbox = selectedTerm.querySelector("input");
     const updatedStatus = selectedCheckbox.checked;
-    if (e.target.tagName === "SPAN") {
-      selectedCheckbox.checked = updatedStatus;
-    }
     if (selectedTerm === allTermsCheck.parentElement) {
-      mendatoryTermsChecks.forEach(check => check.checked = updatedStatus);
-      optionalTermsChecks.forEach(check => check.checked = updatedStatus);
+      mandatoryTermsContainer.forEach(elem => elem.querySelector("input").checked = updatedStatus);
+      optionalTermsContainer.forEach(elem => elem.querySelector("input").checked = updatedStatus);
     } else {
       if (updatedStatus) {
-        const isAllMendatoryTermsChecked = Array.from(mendatoryTermsChecks).filter(check => check !== e.target).every(check => check.checked);
-        const isAllOptionalTermsChecked = Array.from(optionalTermsChecks).filter(check => check !== e.target).every(check => check.checked);
-        if (isAllMendatoryTermsChecked && isAllOptionalTermsChecked) {
+        const isAllMandatoryTermsChecked = Array.from(mandatoryTermsContainer).filter(container => container !== e.target.parentElement.parentElement).every(container => container.querySelector("input").checked);
+        const isAllOptionalTermsChecked = Array.from(optionalTermsContainer).filter(container => container !== e.target.parentElement.parentElement).every(container => container.querySelector("input").checked);
+        if (isAllMandatoryTermsChecked && isAllOptionalTermsChecked) {
+          console.log("need to check all terms")
           // 체크가 된 경우에는 다른 나머지 버튼들이 모두 체크돼어있는지 확인하고 그런 경우에는 전체 동의 체크
           allTermsCheck.checked = updatedStatus;
         }
