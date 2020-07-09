37701b910c612862cd9009a2fb55d925d79bf031
eunjung <jenny.it.kr@gmail.com>
2020-07-09 14:12:40 +0900

    Add tel auth timer
    - 인증하기 버튼을 누르면 2분부터 1초씩 카운트다운 진행
    - 재전송 버튼을 누르면 다시 2분부터 1초씩 카운트다운 진행
    - 인증하기 버튼 누른 후 재전송 버튼을 누를 때마다 인증번호 입력 칸이 추가되는 버그 수정

--- static/js/telAuth.js
+++ static/js/telAuth.js
@@ -1,7 +1,36 @@
 const telAuthContainer = document.querySelector(".tel-auth");
 const telAuthReqContainer = telAuthContainer.querySelector(".tel-auth__req");
 const telAuthReqBtn = telAuthReqContainer.querySelector("button");
-// const telAuthResBtn = telAuthResContainer.querySelector("button");
+
+let timer;
+
+const showTimeLimit = function(timeLimitInString) {
+  const timeLimit = document.querySelector(".tel-auth__res-limit");
+  timeLimit.innerText = timeLimitInString;
+}
+
+const convertTimeLimit = function(seconds) {
+  const min = parseInt(seconds / 60);
+  const sec = seconds % 60;
+  const timeLimitInString = `${min}:${sec > 10 ? sec : `0${sec}`}`
+  showTimeLimit(timeLimitInString);
+}
+
+const setAuthTimer = function() {
+  clearInterval(timer);
+  let limitInSec = 120;
+  timer = setInterval(() => {
+    limitInSec -= 1;
+    convertTimeLimit(limitInSec);
+  }, 1000);
+}
+
+const createAuthTimer = function() {
+  const timeLimit = document.createElement("span");
+  timeLimit.innerText = "2:00";
+  timeLimit.classList.add("tel-auth__res-limit");
+  telAuthContainer.appendChild(timeLimit);
+}
 
 const showTelAuthResContainer = function() {
   const telAuthResContainer = document.createElement("div");
@@ -22,9 +51,13 @@ const convertInnerText = function() {
   telAuthReqBtn.innerText = "재전송";
 }
 
-const handleReqBtnClick = function() {
-  convertInnerText();
-  showTelAuthResContainer();
+const handleReqBtnClick = function(e) {
+  if (e.target.innerText !== "재전송") {
+    convertInnerText();
+    showTelAuthResContainer();
+    createAuthTimer();
+  }
+  setAuthTimer();
 }
 
 telAuthReqBtn.addEventListener("click", handleReqBtnClick);
