7ace5a93de7bc4efe50b8d5c3af78f27d2e4ce7d
eunjung <jenny.it.kr@gmail.com>
2020-07-10 13:05:34 +0900

    Update send login request handling
    - 로그인 성공한 경우 리다이렉션 기능 구현

--- static/js/login.js
+++ static/js/login.js
@@ -24,7 +24,13 @@
         headers: {
           "Content-Type": "application/json"
         }
-      })  
+      }).then(res => {
+        if (res.status === 200) {
+          location.href = "/";
+        } else {
+          alert("입력하신 정보가 올바르지 않습니다.");
+        }
+      }).catch()
     } else {
       alert("아이디와 비밀번호를 모두 입력해주세요.");
     }
